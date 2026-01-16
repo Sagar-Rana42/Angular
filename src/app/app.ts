51fb3dbde4b421bad241da696c1222a2aebfebc5
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-16 17:20:36 +0530

    @switch

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Counter } from './counter/counter';
-import { Event } from './event/event';
+// import { Event } from './event/event';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,13 @@ import { Event } from './event/event';
   styleUrl: './app.css'
 })
 export class App {
-  users = signal(['batman','spider man','super man','iron man','thanos'])  
-  usersDetails = signal([
-  { id: 1,firstName: "Sagar",lastName: "Rana",email: "sagar.rana@example.com"  },
-  {id: 2, firstName: "Amit",lastName: "Sharma",email: "amit.sharma@example.com" },
-  
-  {
-    id: 3,firstName: "Priya",lastName: "Verma",email: "priya.verma@example.com" 
-  },
-  {
-    id: 4, firstName: "Rahul", lastName: "Mehta",email: "rahul.mehta@example.com"
-  },
-  {
-    id: 5, firstName: "Neha", lastName: "Singh", email: "neha.singh@example.com"
+  status = signal('loading')
+
+  handleChange(event:Event){
+    // console.log(event)
+    const value = (event?.target as HTMLInputElement).value;
+    this.status.set(value)
+    // console.log((event.target as HTMLInputElement).value)
   }
-]);
 
 }
