818860673f9253a1819c72f50c6a889beb292651
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-16 10:33:57 +0530

    signal vs state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Counter } from './counter/counter';
 import { Event } from './event/event';
 
 @Component({
   selector: 'app-root',
-  imports: [Event],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,6 +13,23 @@ export class App {
   protected readonly title = signal('angular-basics');
 
   
-  
+  // btnDisable = false
+  data = 10;
+
+  count = signal(30)
+
+  constructor(){
+    effect(()=>{
+      // console.log("this is data " , this.data)
+      console.log("this is count " , this.count())
+    })
+  }
+  updateCount(){
+    this.count.set(this.count() + 1)
+  }
+
+  updateData(){
+    this.data ++;
+  }
 
 }
