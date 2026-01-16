bfe4fbcd87673b13bba4807d129d5221b80f6e1a
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-16 10:09:57 +0530

    event handler

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Counter } from './counter/counter';
+import { Event } from './event/event';
 
 @Component({
   selector: 'app-root',
-  imports: [Counter],
+  imports: [Event],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-basics');
 
+  
   
 
 }
--- src/app/event/event.ts
+++ src/app/event/event.ts
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './event.html',
   styleUrl: './event.css',
 })
-export class Event {
 
+export class Event {
+  input_value = ""
+  
+  handleClickEvent(event:MouseEvent){
+    console.log(event)
+    console.log((event.target as Element).className)
+    alert("button Clicked")
+  }
+  mouseEnterEvent(event:MouseEvent){
+    alert("Mouse enter event trigger")
+  }
+  mouseLeaveEvent(event:MouseEvent){
+    alert("Mouse enter event trigger")
+  }
+  // inputEvent(event:InputEvent){
+  //   // alert(`(event.target as HTMLInputElement).value`)
+  //   console.log(event.target as HTMLInputElement)
+  // }
 }
