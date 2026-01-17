8806a37b6e3bdc66bf620b75e4de1ed0bcd52b3a
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-17 11:09:04 +0530

    two way data binding in object with or without signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,33 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Counter } from './counter/counter';
+import { FormsModule } from '@angular/forms';
 // import { Event } from './event/event';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  status = signal('loading')
 
-  handleChange(event:Event){
-    // console.log(event)
-    const value = (event?.target as HTMLInputElement).value;
-    this.status.set(value)
-    // console.log((event.target as HTMLInputElement).value)
+export class App {
+  name = signal("sagar ")
+  age = 10
+
+  userData = signal({
+    title:"sagar rana",
+    age:24,
+    email:"sagar@gmail.com"
+  })
+  profileData = {
+    title:"rana",
+    age:40,
+    email:"rana@gmail.com"
   }
+  updateUserData(key:string , value:string){
+    this.userData.update((item)=>({...item , [key]:value}))
+  }
+ 
 
 }
