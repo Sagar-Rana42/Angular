9bb3572f0e53c1030ef4ec557811e7fab1cec440
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-17 16:02:00 +0530

    *ngFor

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,15 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { Counter } from './component/counter/counter';
 import { FormsModule } from '@angular/forms';
 import { Api } from './services/api';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule,NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
+
+  /*
   productData:any = signal({})
   constructor(private productService:Api){
 
@@ -23,8 +26,24 @@ export class App {
       // console.log("product data = " ,this.productData().products)
     })
   }
+    */
+ 
+  isLogin = signal(true)
+  users = signal([1,2,3,4])
 
+  students= ["sagar" ,"rana","kumar","jalie"]
+  studentData = [
+    {
+      name:"sagar",
+      age:23,
+      email:"sagar@gmail.com"
+    },
+    {
+      name:"rana",
+      age:43,
+      email:"rana@gmail.com"
+    }
 
-
+  ]
 
 }
