888b91c249466df91367f34812a6738426ef9793
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-17 17:35:05 +0530

    passing data from parent to child

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,18 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { Counter } from './component/counter/counter';
 import { FormsModule } from '@angular/forms';
 import { Api } from './services/api';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule,NgFor],
+  imports: [FormsModule , Child],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-
-  /*
+/*
+  
   productData:any = signal({})
   constructor(private productService:Api){
 
@@ -28,22 +29,9 @@ export class App {
   }
     */
  
-  isLogin = signal(true)
-  users = signal([1,2,3,4])
+ 
+  userName= signal(["sagar","rana","abc","def",'ghi'])
 
-  students= ["sagar" ,"rana","kumar","jalie"]
-  studentData = [
-    {
-      name:"sagar",
-      age:23,
-      email:"sagar@gmail.com"
-    },
-    {
-      name:"rana",
-      age:43,
-      email:"rana@gmail.com"
-    }
 
-  ]
 
 }
