a7606c55596255bc0cb88054e63723698228a944
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2026-01-19 15:17:14 +0530

    routes of pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Profile } from './pages/profile/profile';
+import { About } from './pages/about/about';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:Home},
+    {path:"login" , component:Login},
+    {path:"profile", component:Profile},
+    {path:"about", component:About},
+    {path:"**" , component:PageNotFound}
+];
